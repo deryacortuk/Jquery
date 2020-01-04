4560cd2b5e2554d5db618456c1ce288c2fcd38d0
deryacortuk <ozelderszamani.net@gmail.com>
2020-01-04 23:00:02 +0300

    add code for section

---
+++ util/js/index.js
@@ -0,0 +1,12 @@
+$(function() {
+
+    // $(".purple-box").fadeOut(2000);
+    //  $(".green-box").fadeIn(1000);
+    $(".purple-box").fadeTo(2000, 0.2);
+    $(".green-box").fadeTo(2000, 0.9);
+    $(".blue-box").fadeTo(2000, 0.7);
+
+    $(".blue-box").fadeToggle();
+
+
+});
